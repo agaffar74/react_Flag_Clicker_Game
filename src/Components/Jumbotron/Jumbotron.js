1e7c9b7cbc156796d3b0273a374c9c6930b6a8da
Abdul Gaffar <a_gaffar74@yahoo.com>
2023-03-25 11:57:40 +0000

    jumborton has an update

--- src/Components/Jumbotron/Jumbotron.js
+++ src/Components/Jumbotron/Jumbotron.js
@@ -4,7 +4,7 @@ import "./Jumbotron.css";
 const Jumbotron = props =>
   <div className="jumbotron jumbo-custom">
     <h1 className="display-4">Flags Memory Game</h1>
-    <p className="lead" style={{ color: 'white' }}>Click on a flag to earn points, but don't click on the same flag more than once!</p>
+    <p className="lead" style={{ color: '#ffcc00' }}>Click on a flag to earn points, but don't click on the same flag more than once!</p>
     <hr className="my-4"></hr>
    
   </div>
